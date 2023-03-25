cf47d3a11534483803f4831759f7c06278435ecb
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-03-25 13:37:03 +0300

    add pageController and pageRoute

---
+++ controllers/pageControllers.js
@@ -0,0 +1,12 @@
+exports.getAboutPage = (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
+}
+
+
+exports.getIndexPage = (req, res) => {
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+}
